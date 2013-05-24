dc02ddb6d7ec291aceb4b101e4cf7f031c8aadef
ABobrov <abobrov@mail.ru>
2013-05-24 16:46:40 +0600

    blog1_changelist

--- blog1/urls.py
+++ blog1/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from views import blog_main
+from django.conf import settings
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +20,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+from django.conf import settings
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+         url(r'^static/(?P<path>.*)$', 'serve'),
+    )
